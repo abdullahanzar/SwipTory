490bc1341807b67242e9de22a29dccaeb1600b4d
abdullahanzar <31632960+abdullahanzar@users.noreply.github.com>
2023-07-16 05:09:07 +0530

    Bookmark Fully Implemented

--- SwipTory/src/WebView/HomePage/Components/InfinitySlide.jsx
+++ SwipTory/src/WebView/HomePage/Components/InfinitySlide.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./InfinitySlide.css";
 import axios from "axios";
-import leftSlide from './assets/leftSlide.png';
-import rightSlide from './assets/rightSlide.png';
-import saveSlide from './Assets/saveSlide.png';
-import shareSlide from './Assets/shareSlide.png';
-import exitSlide from './Assets/existSlide.png'
+import leftSlide from "./assets/leftSlide.png";
+import rightSlide from "./assets/rightSlide.png";
+import saveSlide from "./Assets/saveSlide.png";
+import savedSlide from "./Assets/savedSlide.png";
+import shareSlide from "./Assets/shareSlide.png";
+import exitSlide from "./Assets/existSlide.png";
 import SignUpForm from "./SignUpForm";
 import ReactModal from "react-modal";
 
@@ -15,10 +16,11 @@ export default function InfinitySlide(props) {
   const [iteration, setIteration] = useState(props.storyID);
   const [lastSlide, setLastSlide] = useState(0);
   const [intervalID, setIntervalID] = useState(-1);
-  const [bookmarkChng, setBookmarkChng] = useState("")
+  const [bookmarkChng, setBookmarkChng] = useState("");
   useEffect(() => {
     (async () => setdisplayStory(await fetchStoryByID(iteration)))();
-    clearInterval(intervalID)
+    clearInterval(intervalID);
+    isBookmarked(iteration, setBookmarkChng)
   }, [iteration]);
   useEffect(() => {
     const interval = [];
@@ -31,8 +33,7 @@ export default function InfinitySlide(props) {
         displayStory.forEach((item, index) => {
           const timeout = setTimeout(() => {
             setCurrentSlide({ ...item });
-            if(index+1==displayStory.length)
-            setLastSlide(index);
+            if (index + 1 == displayStory.length) setLastSlide(index);
           }, index * 3000);
           timeouts.push(timeout);
         });
@@ -46,40 +47,49 @@ export default function InfinitySlide(props) {
     } else {
       displayStory.map((item, index) => {
         setCurrentSlide(item);
-        const intervalID = setInterval(()=>{
-            setIteration(prev=>prev+1);
-        }, 3000)
+        const intervalID = setInterval(() => {
+          setIteration((prev) => prev + 1);
+        }, 3000);
         interval.push(intervalID);
-        setIntervalID(intervalID)
-        return ()=>clearInterval(interval[0])
+        setIntervalID(intervalID);
+        return () => clearInterval(interval[0]);
       });
     }
-    return ()=>clearInterval(interval[0])
+    return () => clearInterval(interval[0]);
   }, [displayStory]);
-  useEffect(()=>{
-    const interval=[];
-    if(lastSlide!=0) {
-        const intervalID = setInterval(()=>{
-            setIteration(prev=>prev+1);
-        }, 3000)
-        interval.push(intervalID);
-        setIntervalID(intervalID)
+  useEffect(() => {
+    const interval = [];
+    if (lastSlide != 0) {
+      const intervalID = setInterval(() => {
+        setIteration((prev) => prev + 1);
+      }, 3000);
+      interval.push(intervalID);
+      setIntervalID(intervalID);
     }
-    return ()=>clearInterval(interval[0])
-  }, [lastSlide])
-  useEffect(()=>{
-    if(bookmarkChng=="Not logged in") {
-        props.setToLogIn(true);
-        props.setClose(false);
+    return () => clearInterval(interval[0]);
+  }, [lastSlide]);
+  useEffect(() => {
+    if (bookmarkChng == "Not logged in") {
+      props.setToLogIn(true);
+      props.setClose(false);
     }
-  }, [bookmarkChng])
+    console.log(bookmarkChng);
+  }, [bookmarkChng]);
   return (
     <div className="infinitySlides">
-        {(intervalID!==-1) && <div className="slider">
-            <div className="sliding"></div>
-          </div>}
+      {intervalID !== -1 && (
+        <div className="slider">
+          <div className="sliding"></div>
+        </div>
+      )}
       {
-        <div className="slide" onClick={()=>{clearInterval(intervalID);setIntervalID(-1)}}>
+        <div
+          className="slide"
+          onClick={() => {
+            clearInterval(intervalID);
+            setIntervalID(-1);
+          }}
+        >
           <p>
             {currentSlide.heading}
             <br />
@@ -89,14 +99,38 @@ export default function InfinitySlide(props) {
         </div>
       }
       <div className="leftslider">
-        <img src={leftSlide} alt="LEFT" onClick={()=>{setIteration(prev=>prev-1)}}/>
+        <img
+          src={leftSlide}
+          alt="LEFT"
+          onClick={() => {
+            setIteration((prev) => prev - 1);
+          }}
+        />
       </div>
       <div className="rightslider">
-        <img src={rightSlide} alt="RIGHT" onClick={()=>{setIteration(prev=>prev+1)}}/>
+        <img
+          src={rightSlide}
+          alt="RIGHT"
+          onClick={() => {
+            setIteration((prev) => prev + 1);
+          }}
+        />
       </div>
-      <div className="bookmark" onClick={()=>{setBookmark(iteration, setBookmarkChng)}}>
-        <img src={saveSlide} alt="Save" />
-      </div>
+      {bookmarkChng == "Bookmarked" ? (
+        <div className="bookmark">
+          <img src={savedSlide} alt="Saved" />
+        </div>
+      ) : (
+        <div
+          className="bookmark"
+          onClick={() => {
+            if(bookmarkChng!=='Bookmarked')
+            setBookmark(iteration, setBookmarkChng);
+          }}
+        >
+          <img src={saveSlide} alt="Save" />
+        </div>
+      )}
     </div>
   );
 }
@@ -113,16 +147,54 @@ async function fetchStoryByID(storyID) {
 }
 
 async function setBookmark(storyID, setBookmarkChng) {
-    try {
-        const payload = {
-            user: localStorage.getItem('user'),
-            storyID: storyID
-        }
-        const response = await axios.post('https://swiptory.onrender.com/bookmark');
-        if(response.data?.error=="Sign In First")
-        setBookmarkChng("Not logged in")
-    }
-    catch (e) {
-        console.log(e);
-    }
+  try {
+    const payload = {
+      username: localStorage.getItem("user"),
+      storyID: storyID,
+    };
+    const response = await axios.post(
+      "https://swiptory.onrender.com/bookmark",
+      payload,
+      {
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          token: localStorage.getItem("token"),
+        },
+      }
+    );
+    console.log(response);
+    if (response.data?.error == "Sign In First")
+      setBookmarkChng("Not logged in");
+    if (response.data?.username == localStorage.getItem("user"))
+      setBookmarkChng("Bookmarked");
+    if (response.data?.error == "Bookmark already exists. Try delete request.")
+      setBookmarkChng("Bookmarked");
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function isBookmarked(storyID, setBookmarkChng) {
+  try {
+    const payload = {
+      username: localStorage.getItem("user"),
+      storyID: storyID,
+    };
+    const response = await axios.post(
+      "https://swiptory.onrender.com/bookmark",
+      payload,
+      {
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          token: localStorage.getItem("token"),
+        },
+      }
+    );
+    if (response.data?.error == "Bookmark already exists. Try delete request.")
+      setBookmarkChng("Bookmarked");
+    else 
+      setBookmarkChng("")
+  } catch (e) {
+    console.log(e);
+  }
 }
